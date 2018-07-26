cf88d0807e5800973bf8959ac5d4939498f07291
Serigne <seloweb810@gmail.com>
2018-07-26 21:01:30 +0000

    add database file

--- view/header_view.php
+++ view/header_view.php
@@ -11,21 +11,9 @@
     <div id="nav" class="navbar navbar-inverse navbar-fixedtop" role="navigation">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" aria-controls="hl-nav" aria-expanded="true" data-toggle="collapse" data-target="#hl-nav">
-                    <span class="sr-only">Admin</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    
-                </button>
-                <a class="navbar-brand" href=".?action=">Admin</a>
+                <a class="navbar-brand" href=".?action=">Symple PHP App</a>
             </div>
-            <div class="navbar-collapse collapse out" id="hl-nav" aria-expanded="true">
-                <ul class="nav navbar-nav navbar-right">
-                    <li id="nav-home"><a href=".?action=categories">Home</a></li>
-                </ul>
-
-            </div>
+           
         </div>
     </div>
     
